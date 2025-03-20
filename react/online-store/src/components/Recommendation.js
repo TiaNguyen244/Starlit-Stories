65c5f65c729487dc2fbcb83869c62a257cc20ebb
Kasey S <kasey@webage.com>
2025-03-20 15:49:15 -0400

    update recommendations

--- react/online-store/src/components/Recommendation.js
+++ react/online-store/src/components/Recommendation.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react";
 import Book from "./Book.js"
 
+import { Link } from "react-router-dom";
+
 function Recommendation(props) {
   const [data, setData] = useState([]);
+  const [books, setBooks] = useState([]);
+
+
+
+
   let authorInd=0
   let titleInd =1
   let genreInd=2
   let subgenreInd =3
   let priceInd =4
+  let yearInd =5
   let ratingInd = 6
   let isbnInd = 7
-  let decrInd = 8
+  let descrInd = 8
 
 
 
@@ -30,15 +38,71 @@ function Recommendation(props) {
       } catch (error) {
         console.error("Error fetching recs:", error);
       }
+
+      try {
+        const response = await fetch(`http://localhost:3000/books/`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch book details");
+        }
+        const data = await response.json();
+        setBooks(data);
+      } catch (error) {
+        console.error("Error fetching book details:", error);
+      }
+
     };
 
     fetchData();
   }, []);  
 
+
+  const getIDfromISBN =  (isbn) =>{
+    let book = books.filter((object) => object.ISBN == isbn);
+    console.log(book)
+    return book[0]?._id;
+
+  }
+
+
   return (
     <>
       <h3>Interested in more books? Check out these titles!</h3>
-      {}
+      <div className="row">
+        {data.map((book) => (
+          <div className="col">
+            <div className="card h-100">
+              <div className="text-center pt-3">
+                <img
+                  src="/placeholder.svg"
+                  className="card-img-top"
+                  alt={book.title}
+                  style={{
+                    height: "200px",
+                    width: "100%",
+                    objectFit: "contain",
+                  }}
+                />
+              </div>
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title">{book[titleInd]}</h5>
+                <p className="card-text text-muted">by {book[authorInd]}</p>
+                <p className="card-text">
+                  <small className="text-muted">{book[genreInd]}</small>
+                  <small className="text-muted">: {book[subgenreInd]}</small>
+                </p>
+                {/* <p className="card-text">{book[descrInd]}</p> */}
+                <p className="card-title">Price: ${book[priceInd]}</p>
+                <Link
+                  to={`/books/${getIDfromISBN(book[isbnInd])}`}
+                  className="btn btn-primary mt-auto"
+                >
+                  View Details
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
